4567437b4b76a986213025bd00aa08fb4989dd6f
Alison <alisonrt@gmail.com>
2021-06-13 16:23:03 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-        <footer>
-        This project was coded by Alison and is{" "}
-          <a href="https://github.com/alisonrt/react-project" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
-        </footer>
+      <div className="container">
+        Weather App
+          <footer>
+          This project was coded by Alison and is{" "}
+            <a href="https://github.com/alisonrt/react-project" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+          </footer>
+      </div>
     </div>
   );
 }
