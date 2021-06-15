658b4962e4471cd23ebd8ed79079c4e173f30f47
Alison <alisonrt@gmail.com>
2021-06-15 09:15:21 -0700

    added weather API and fields

--- src/Weather.js
+++ src/Weather.js
@@ -1,40 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import "./Weather.css";
 
-export default function Weather () {
-    return(
-        <div className="Weather">
-            <form>
-                <div className="row">
-                    <div className="col-9">
-                        <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on"/>
-                    </div>
-                    <div className="col-3">
-                        <input type="submit" value="Search" className="btn btn-primary w-100" />
-                    </div>
-                </div>
-            </form>
-            <h1>Seattle</h1>
-            <ul>
-                <li>Sunday 16:35</li>
-                <li>Rain</li>
-            </ul>
-            <div className="row mt-3">
-                <div className="col-6">
-                    <div className="clearfix">
-                        <img src="https://ssl.gstatic.com/onebox/weather/64/rain.png" alt="rain" className="float-left"/>
-                        <span className="temperature">16</span><span className="unit">˚C</span>
-                        
-                    </div>
-                </div>
-                <div className="col-6">
+export default function Weather (props) {
+    const [weatherData, setWeatherData] = useState({ready: false});
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            humidity: response.data.main.humidity,
+            description: response.data.weather[0].description,
+            iconUrl:"https://ssl.gstatic.com/onebox/weather/64/rain.png",
+            wind: response.data.wind.speed,
+            city: response.data.name,
+            date: "Wednesday 04:35"
+        });
+    }
+
+    if (weatherData.ready) {
+        return(
+                <div className="Weather">
+                    <form>
+                        <div className="row">
+                            <div className="col-9">
+                                <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on"/>
+                            </div>
+                            <div className="col-3">
+                                <input type="submit" value="Search" className="btn btn-primary w-100" />
+                            </div>
+                        </div>
+                    </form>
+                    <h1>{weatherData.city}</h1>
                     <ul>
-                        <li>Precipitation: 97%</li>
-                        <li>Humidity: 95%</li>
-                        <li>Wind: 3 km/h</li>
+                        <li>{weatherData.date}</li>
+                        <li className="text-capitalize">{weatherData.description}</li>
                     </ul>
+                    <div className="row mt-3">
+                        <div className="col-6">
+                            <div className="clearfix">
+                                <img src={weatherData.iconUrl} alt={weatherData.description} className="float-left"/>
+                                <span className="temperature">{Math.round(weatherData.temperature)}</span><span className="unit">˚C</span>
+                                
+                            </div>
+                        </div>
+                        <div className="col-6">
+                            <ul>
+                                <li>Humidity: {weatherData.humidity}%</li>
+                                <li>Wind: {weatherData.wind} km/h</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    );
+            );
+    } else {
+    const apiKey = "20f80f60d74acf5419e80528f290a5b9";
+    let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${props.dafaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "loading...";
+    }
+    
 }
