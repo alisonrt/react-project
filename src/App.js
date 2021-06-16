193d9b0411fa6ec6b08956747224619fadb6b840
Alison <alisonrt@gmail.com>
2021-06-16 15:46:37 -0700

    fixed typos

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather deafultCity="Seattle" />
+        <Weather defaultCity="Seattle" />
           <footer>
           This project was coded by Alison and is{" "}
             <a href="https://github.com/alisonrt/react-project" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
