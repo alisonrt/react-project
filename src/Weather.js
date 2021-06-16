213ee241accd1be3fbc1d26bc662019ac1b643a1
Alison <alisonrt@gmail.com>
2021-06-16 15:26:39 -0700

    added date function

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import prettyDate from './prettyDate';
 import "./Weather.css";
 
 export default function Weather (props) {
@@ -14,7 +15,7 @@ export default function Weather (props) {
             iconUrl:"https://ssl.gstatic.com/onebox/weather/64/rain.png",
             wind: response.data.wind.speed,
             city: response.data.name,
-            date: "Wednesday 04:35"
+            date: new Date(response.data.dt * 1000)
         });
     }
 
@@ -33,7 +34,7 @@ export default function Weather (props) {
                     </form>
                     <h1>{weatherData.city}</h1>
                     <ul>
-                        <li>{weatherData.date}</li>
+                        <li><prettyDate date={weatherData.date} /></li>
                         <li className="text-capitalize">{weatherData.description}</li>
                     </ul>
                     <div className="row mt-3">
